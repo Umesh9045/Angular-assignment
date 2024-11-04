bdf43186008bb22c8531fd30599c9b47152fed1c
Umesh9045 <umeshsonawane9045@gmail.com>
2024-11-04 17:55:39 +0530

    added navbar and updated sidebar ui and fix toggle of open and close

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,19 @@ import { Component } from '@angular/core';
 import { RouterLinkActive, RouterLink, RouterOutlet } from '@angular/router';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterLinkActive, RouterLink, RouterOutlet, MatSidenavModule, MatListModule],
+  imports: [RouterLinkActive, RouterLink, RouterOutlet, MatSidenavModule, MatToolbarModule, MatButtonModule, MatIconModule, MatListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-assignment';
+  showFiller = false;
 }
 
