d0e433fe39ab5f3495f77ec4dfaf4ba87a5bc360
Umesh9045 <umeshsonawane9045@gmail.com>
2024-11-05 14:41:16 +0530

    css inhancement, added cusor pointer to the btns

--- src/app/module/products-master/delete-product/delete-product.component.ts
+++ src/app/module/products-master/delete-product/delete-product.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogRef } from '@angular/material/dialog';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-delete-product',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatTooltipModule],
   templateUrl: './delete-product.component.html',
   styleUrl: './delete-product.component.css'
 })
